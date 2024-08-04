fc23da1b6b8fe09a313ece4a1f0338542297f3d1
Dyneteve <dyneteve@hentaios.com>
2024-08-04 11:26:34 +0800

    lmi: Import gboard paddings from pixel7
    Change-Id: Ie0f15bc6a8aad6c425fef445ec420c8ee27d9612
    Signed-off-by: Omkar Chandorkar <gotenksIN@aospa.co>
    Signed-off-by: Alexey Shelby <shelbyhell@proton.me>

--- device.mk
+++ device.mk
@@ -170,6 +170,12 @@ PRODUCT_VENDOR_PROPERTIES += \
 PRODUCT_VENDOR_PROPERTIES += \
     vendor.gatekeeper.disable_spu=true
 
+# Gboard paddings
+# Keyboard height ratio and bottom padding in dp for portrait mode
+PRODUCT_PRODUCT_PROPERTIES += \
+    ro.com.google.ime.height_ratio=1.075 \
+    ro.com.google.ime.kb_pad_port_b=8
+    
 # Health
 PRODUCT_PACKAGES += \
     android.hardware.health@2.1-impl-qti \
