096b0218fd3493e857c0aed18f64925012a8ed67
Fabian Leutenegger <fabian.leutenegger@bluewin.ch>
2024-09-26 10:00:03 +0200

    lmi: Allow gettid syscall for wfdhdcphalservice
    09-25 22:37:52.968 16288 16288 E wfdhdcphal: wfdhdcphalservice starting...
    09-25 22:37:52.970 16288 16288 W wfdhdcphalservice: libminijail[16288]: failed to get path of fd 5: No such file or directory
    09-25 22:37:52.970 16288 16288 W wfdhdcphalservice: libminijail[16288]: allowing syscall: connect
    09-25 22:37:52.970 16288 16288 W wfdhdcphalservice: libminijail[16288]: allowing syscall: fcntl
    09-25 22:37:52.970 16288 16288 W wfdhdcphalservice: libminijail[16288]: allowing syscall: sendto
    09-25 22:37:52.970 16288 16288 W wfdhdcphalservice: libminijail[16288]: allowing syscall: socket
    09-25 22:37:52.970 16288 16288 W wfdhdcphalservice: libminijail[16288]: allowing syscall: writev
    09-25 22:37:52.970 16288 16288 W wfdhdcphalservice: libminijail[16288]: compile_file: <fd>(33): nonexistent syscall 'fstatat'
    09-25 22:37:52.970 16288 16288 W wfdhdcphalservice: libminijail[16288]: compile_file: <fd>(52): nonexistent syscall 'open'
    09-25 22:37:52.970 16288 16288 W wfdhdcphalservice: libminijail[16288]: compile_file: <fd>(53): nonexistent syscall 'ugetrlimit'
    09-25 22:37:52.970 16288 16288 W wfdhdcphalservice: libminijail[16288]: compile_file: <fd>(64): nonexistent syscall 'readlink'
    09-25 22:37:52.970 16288 16288 W wfdhdcphalservice: libminijail[16288]: logging seccomp filter failures
    09-25 22:37:52.971 16288 16288 E wfdhdcphalservice: libminijail[16288]: blocked syscall: gettid
    09-25 22:37:52.987     1     1 I init    : Service 'wfdhdcphalservice' (pid 16288) received signal 31
    09-25 22:37:52.987     1     1 I init    : Sending signal 9 to service 'wfdhdcphalservice' (pid 16288) process group...
    
    Change-Id: I9b33dd63b408444f26382aaf2b7647ae00500c08
    Signed-off-by: chrisl7 <wandersonrodriguesf1@gmail.com>

--- extract-files.sh
+++ extract-files.sh
@@ -59,7 +59,8 @@ function blob_fixup() {
         vendor/etc/init/init.batterysecret.rc)
             sed -i "/seclabel u:r:batterysecret:s0/d" "${2}"
             ;;
-        vendor/etc/seccomp_policy/atfwd@2.0.policy)
+        vendor/etc/seccomp_policy/atfwd@2.0.policy|vendor/etc/seccomp_policy/wfdhdcphalservice.policy)
+            [ -n "$(tail -c 1 "${2}")" ] && echo >> "${2}"
             grep -q "gettid: 1" "${2}" || echo "gettid: 1" >> "${2}"
             ;;
     esac
